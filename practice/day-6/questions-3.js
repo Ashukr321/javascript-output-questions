f3294c085ab300325dac39c84de4491a2a0e58cf
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-10-27 06:41:04 +0530

    feat(day-6):ques 3 function decl.

---
+++ practice/day-6/questions-3.js
@@ -0,0 +1,14 @@
+let x = 10;
+function print() {
+  console.log(x);
+}
+// variable  lookup  , 
+// it goes to the ge , variable env. from there it will get the value 
+// it will get the value of x 
+
+
+{
+  let x = 20; // let and const are the block scope
+  print();
+}
+
