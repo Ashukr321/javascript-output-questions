23773f593b2be3a445fa2b62c3e64cee750a6161
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-11-09 22:19:13 +0530

    feat(day -7 ) complete questions 1

---
+++ practice/day-7/questions-1.js
@@ -0,0 +1,11 @@
+sayHello();  // Hello JS
+
+// function declaration: 
+// fd is hoisted in nature
+function sayHello() {
+  console.log("Hello JS");
+}
+
+// logic : when function declare is hoisted it goes in top of the global environment variable function body also
+// due this region we can call before declaration 
+
