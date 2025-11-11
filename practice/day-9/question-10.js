712431ccf8904671717ab94ac832661275838b4c
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-11-11 10:42:24 +0530

    feat (string) :split methods

--- practice/day-9/question-10.js
+++ practice/day-9/question-10.js
@@ -1,1 +1,12 @@
 
+let str = "a+b+c+d"; // 
+console.log(str.split("+", 2));
+// split methods convert the string to array based on the symbol
+
+// split("seperator",limit)
+// ['a','b']
+
+
+// split methods  convert the string to array 
+
+
--- practice/day-9/question-9.js
+++ practice/day-9/question-9.js
@@ -4,9 +4,8 @@ console.log(str.trimStart().length); // 7
 console.log(str.trimEnd().length); //  6
 console.log(str.trim().length);  // 4
 
-// trimStart() remove the extra spaces from the starting of the string 
-// trimEnd() > remove the  extra spaces from end of the string 
+// trimStart() remove the extra spaces from the starting of the string
+// trimEnd() > remove the  extra spaces from end of the string
 // length > it remove the all spaces from the starting and end of the string 
 
 
- 
