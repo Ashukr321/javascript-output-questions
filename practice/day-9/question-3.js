cd0584e2e823f7d50594e03494b41e0dc3edc079
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-11-11 10:07:27 +0530

    feat(async)

--- practice/day-9/question-3.js
+++ practice/day-9/question-3.js
@@ -1,1 +1,17 @@
+const p1 = new Promise((resolve) => {
+  console.log("Promise 1");
+  setTimeout(() => {
+    resolve("Resolved 1");
+  }, 0);
+});
 
+
+const p2 = p1.then((res) => {
+  console.log(res);
+  return "Resolved 2";
+});
+
+
+p2.then((res) => console.log(res));
+
+console.log("End");
--- practice/day-9/question-4.js
+++ practice/day-9/question-4.js
@@ -1,1 +1,11 @@
 
+async function asyncFunc() {
+  console.log("1");
+  await Promise.resolve();
+  console.log("2");
+}
+
+
+console.log("Start");
+asyncFunc();
+console.log("End");
