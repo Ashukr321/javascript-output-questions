ac999d22f4288562004da30830e26113b7171541
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-11-11 10:15:41 +0530

    feat(string)mutable

--- practice/day-9/question-6.js
+++ practice/day-9/question-6.js
@@ -1,1 +1,7 @@
 
+// string is the primitive data -> this is the immutable 
+let str = "hello"; 
+str[0] = "H";// change the h -> H 
+console.log(str); // Hello ❌ , hello 
+// string is the primitive data not mutate the string value h -> H
+ 
