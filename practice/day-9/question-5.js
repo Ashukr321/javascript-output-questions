b8090ebfeb84d048162a0d385ffe4acda1a75bef
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-11-11 10:11:25 +0530

    feat promise and timer

--- practice/day-9/question-5.js
+++ practice/day-9/question-5.js
@@ -1,1 +1,22 @@
 
+setTimeout(() => console.log("setTimeout"), 0);
+
+Promise.resolve().then(() => {
+  console.log("Promise 1");
+  return Promise.resolve();
+}).then(() => {
+  console.log("Promise 2");
+}).then(() => {
+  setTimeout(() => console.log("setTimeout inside Promise"), 0);
+}).then(() => {
+  console.log("Promise 3");
+});
+
+
+
+
+// 1 
+// 2 
+// 3 
+// setTimeout 
+// setTimeOut inside Promise
