b0ac8645364c9af5ab63ebaab1f7c2a255aa6eea
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-11-12 06:46:47 +0530

    feat array join methods

--- practice/day-10/questions-8.js
+++ practice/day-10/questions-8.js
@@ -1,1 +1,7 @@
 
+let s = "  hello   world  ";
+let result = s.trim().split(/\s+/).reverse().join(" ");
+console.log(result); // reverse the string 
+
+// /\s+/ regular string expression 
+// join methods is use to combined array to single string 
