ea37c142bd0d760862c5f832609172378060abe8
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-11-12 06:35:19 +0530

    feat(+ precedence associative

--- practice/day-10/questions-5.js
+++ practice/day-10/questions-5.js
@@ -1,1 +1,6 @@
 
+let text = "5" + 5 + 5; // 555 // 5 converted into string then "5"+"5"+"5" == 555
+let num = 5 + 5 + "5"; // 10+"5"// 10 is converted into string then concat = 105
+console.log(text);
+console.log(num);
+//  + operator evaluate from left to right 
