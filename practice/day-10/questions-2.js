1d40bbc7ee3d6bcaa818e161fd7cd34720c0c7c3
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-11-12 06:25:20 +0530

    feat indexOf

--- practice/day-10/questions-2.js
+++ practice/day-10/questions-2.js
@@ -1,1 +1,8 @@
 
+let str = "banana"; // string literal 
+console.log(str.indexOf("a", 2));
+// indexOf(searchValue , and startIndex) // 3
+console.log(str.lastIndexOf("a")); // 5 
+
+
+// indexOf(search value,startIndexOf);
