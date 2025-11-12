d389e47f6f1af0060024a52ff3df949e455f786d
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-11-12 06:54:46 +0530

    dom innerText

--- practice/day-10/questions-10.js
+++ practice/day-10/questions-10.js
@@ -1,1 +1,8 @@
 
+
+document.getElementById("msg").innerText = "Hi";
+console.log(document.getElementById("msg").innerText);
+
+
+// innerTest replace the prev text this is use for the read and write operator 
+// textContent : return hidden text also 
--- practice/day-10/questions-9.js
+++ practice/day-10/questions-9.js
@@ -1,1 +1,5 @@
 
+let s1 = "listen";
+let s2 = "silent";
+console.log(s1.split("").sort().join("") === s2.split("").sort().join("")); //true 
+// sort on array sort based on the asci value 
