c56aa216f52c8550ba5b4c0e9e032d45d78b4e1c
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-11-12 06:31:08 +0530

    string concat method

--- practice/day-10/questions-4.js
+++ practice/day-10/questions-4.js
@@ -1,1 +1,6 @@
 
+let str = "abc"; // 
+console.log(str.concat("def").toUpperCase()); // ABCDEF
+console.log(str);//abc bec string is the immutable
+// string concat methods is use to combined two string 
+
