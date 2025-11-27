34dbd4e27e60e6c82b12752de9e586c523d7318d
ashukr321 <ashutoshkumarsingh55555@gmail.com>
2025-11-27 09:19:20 +0530

    spread operator sum

--- practice/day-12/questions-6.js
+++ practice/day-12/questions-6.js
@@ -1,3 +1,7 @@
 console.log([1, 2, ...[3, 4]]);
 // spread operator expand any iterables into indiv. array items 
 // 1 2 3 4
+
+// spread=> expad 
+// rest -> just work like packing system 
+// rest param always comes at ends of arguments 
