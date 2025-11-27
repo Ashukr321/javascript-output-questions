d13036a33ed6e05c9b9315e982a59fb6d00e7198
ashukr321 <ashutoshkumarsingh55555@gmail.com>
2025-11-27 09:22:30 +0530

    feat( - , + ) : operator working

---
+++ practice/day-12/questions-7.js
@@ -0,0 +1,11 @@
+let x = "5"; // string immutbale 
+let y = 2; // y variable number 
+console.log(x - y, x + y);
+
+// - operator convert both operands into number then evaluate it 5 - 2 = 3 
+// + convert y in to string  if both are number then do addition 
+
+// 52 
+// 5 - 2 , 
+// 3,52
+// 5+ 2 
