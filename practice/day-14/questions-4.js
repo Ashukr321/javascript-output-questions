8602bf34602726417a8c2eb52342945b07936eac
ashukr321 <ashutoshkumarsingh55555@gmail.com>
2025-12-03 07:33:45 +0530

    feat ( working of sort methods )

---
+++ practice/day-14/questions-4.js
@@ -0,0 +1,11 @@
+// // const arr = [10, 2, 30];
+// arr.sort(); // simple sort methods sort the string array based on the alphabetical
+
+// console.log(arr);
+
+const arr = [10, 5, 20];
+arr.sort((a, b) => b - a);
+
+// 
+console.log(arr);
+
