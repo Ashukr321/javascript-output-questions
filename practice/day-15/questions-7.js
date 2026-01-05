d6a5053e09721c59556aa7f12600d36b329ad89b
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2026-01-05 07:47:04 +0000

    feat(js output):add two array using +

---
+++ practice/day-15/questions-7.js
@@ -0,0 +1,10 @@
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const result = arr1 + arr2;
+
+console.log(typeof result);
+console.log(result);
+// + operator convert into array into string using 
+// toString() method 
+// "1 2 34 5 6"
