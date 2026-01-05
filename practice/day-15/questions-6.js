a38565d3d031866e4d6ac2f2c14e4040c9a33153
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2026-01-05 05:16:21 +0000

    feat(js output ): complete the js output

--- practice/day-15/questions-6.js
+++ practice/day-15/questions-6.js
@@ -1,9 +1,13 @@
 let fruits = ["apple", "banana"]; // array 
-let tropical = fruits; // reference assign in the tropical variable 
+// let tropical = fruits; // reference assign in the tropical variable 
+
+let tropical = [...fruits]; // reference assign in the tropical variable 
+// here we use the spread operator which is basically spread all the element of the array fruites 
+
 
 tropical.push("mango"); // this mutate the orignal fruites  means it will add mango in the fruits 
 
-console.log(fruits); // ["apple","banana","mango"];
+console.log(fruits); // ["apple","banana"];
 console.log(tropical); //  ["apple","banana","mango"]
 
 // because object is the reference data type 
