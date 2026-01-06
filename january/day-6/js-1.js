6aecb1c012b3df1d1112e8c48e66970858ac4bd5
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2026-01-06 06:43:23 +0000

    feat(js output ) : working of the sort methods

--- january/day-6/js-1.js
+++ january/day-6/js-1.js
@@ -1,1 +1,15 @@
-console.log("questions 1")
+const number = [3,41,34,314,31,43,5,5];
+console.log(number.sort()); // this is the sort methods without camparison callback methods 
+// if we have the string items array which we apply simple sort methods then it sort based on the 
+// simple chracter basic
+
+// example - 2
+const fruits = ["mango","apple","banana"];
+console.log(fruits.sort());
+
+
+// 2 ways to sort the data using compare callback funtion 
+// this compare function return a value this tell the js engine how to compare and make the order
+// of the element 
+
+
