bdb62abe489375570ba725d5468f9b458ca0223c
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2026-01-09 06:51:38 +0000

    feat(js hoisted functon declaration funtion

--- january/day-9/js-5.js
+++ january/day-9/js-5.js
@@ -5,3 +5,17 @@ var a = 5; // hoisted in nature
     var a = 10;
     console.log(a); // 10  ✅
 })();
+
+var x = 10;
+
+// this is the fd : this move to top of the code at compilation phase of the js code
+function test() {
+    // in this scope 
+    // x is not found : 
+  if (x > 20) {
+    var x = 50;
+  }
+  console.log(x); // undefiend
+}
+
+test(); 
