a7b5cbe9282cea1fabfac42f88aeb4118ebfdbf4
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2026-01-12 07:52:28 +0000

    feat:  use case of the call methods

---
+++ january/day-12/js-2.js
@@ -0,0 +1,31 @@
+// use case of the call methods 
+// 2 object  of the  person 
+// we both  have need to introduced them self 
+// i want to create a intruduced function which we can reuse for both 
+
+const person1 = {
+    name :"Ashutosh",
+    age:20,
+    address:{
+        city:"patna"
+    }
+}
+const perosn2 = {
+    name:"Alice",
+    age:21,
+    address:{
+        city:"siwan",
+        pin_code:"841238",
+    }
+}
+
+// create the  function   declration 
+function intro (greeting){
+    console.log(`${greeting} -   ${this.name}`);
+}
+
+intro.call(person1,"good morning !");// person1 this is the object referece we pass ;
+
+// here what happend basically the .call methods bind to the introd methods 
+
+intro.call(perosn2,"good evening !");
