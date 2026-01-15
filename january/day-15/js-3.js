a470d56aa3a2f3ce8209ebc623cfaf8e89a1532e
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2026-01-15 12:41:04 +0000

    feat:js output based on the arrow function

---
+++ january/day-15/js-3.js
@@ -0,0 +1,32 @@
+// const button = document.querySelector('button');
+
+// button.addEventListener('click', function() {
+//   console.log(this); // button  node 
+// });
+
+// this in event listern refers to the that event emitter object 
+
+// in the arrow function the value of the this capture from their lexical scope
+
+//object literals
+const user = {
+  name: 'John',
+  // Regular Function
+  // function declaration
+  greet: function() {
+    console.log("Regular:", this.name);
+  },
+  // Arrow Function
+  shout: function(){
+    // here this refer to the object
+    const innerArrow = ()=>{
+        // this inherited
+        console.log("Nested Arrow:", this.name);
+    }
+    innerArrow();
+  }
+  
+};
+
+user.greet(); // Regular: John
+user.shout(); // Arrow: undefined
