1acf056e5fdb51947bca25723394a5a84652e028
Ashukr321 <coderashukr321@gmail.com>
2026-04-03 00:44:04 +0530

    feat(git cmd):understand git push --set--upstream origin dev

--- April/_03_04_26/q1.js
+++ April/_03_04_26/q1.js
@@ -1,2 +1,17 @@
 console.log("03 04 26");
 // ---------------------------------
+
+// understand the git most important cmd 
+// git push --set--upstream origin dev : 
+// this cmd is most important cmd this is use for push the my local 
+// code into remote github  origin : is the connection name that we provide 
+// and --set--upstream is the : connection   it connect out local dev branch 
+// to remote dev branch :
+
+
+// ----------------------------------------------
+` 
+1. git push --set--upstream origin dev 
+2. git push -u origin dev 
+`
+// ----------------------------------
