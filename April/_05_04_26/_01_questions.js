a6499ba3bf0b688270116b1e8a36698ac5c0c4e2
Ashukr321 <coderashukr321@gmail.com>
2026-04-05 08:39:24 +0530

    feat(internal coercion): + , -  operator

--- April/_05_04_26/_01_questions.js
+++ April/_05_04_26/_01_questions.js
@@ -16,4 +16,34 @@ console.log(0==false);  // false => 0 // true
 console.log(0===false); // value is same but both data type is different : 
 // 0 : number 
 // false : boolean 
+// all the object is store inside the heap  in  js engine 
+
+// == : this will do type coercion: false is 0 is converted into false : then compare with value 
+
+// callback queue : 
+// 1. macrotask queue 
+// 2. microtask queue :
+
+console.log("Start");
+setTimeout(()=>console.log("timeout"),2000);
+Promise.resolve().then(()=>console.log("promise resolve"));
+console.log("End");
+
+// output will be : 
+// start 
+// end 
+// primise resolve 
+// timeout 
+// microtask queue has the higher priority then the macrotask 
+// all the timeout api has low priority then promises : 
+
+console.log(typeof []); // Object 
+// array  is object 
+
+//  internal coercion 
+console.log("1"-1);// 0 
+console.log("234"+3);// 2343
+// - : it convert string into number 
+// + : it will concatenate the number into string : 
+
 
